0478f14fdab106f849432fbcfb9387ef05494cc1
marcelduran <marcelduran@gmail.com>
2013-01-14 13:02:56 -0800

    added jasmine tests

--- tests/paypalcalc_spec.js
+++
@@ -1,8 +0,0 @@
-describe('paypal calculator', function () {
-    it('sends 100 USD from BR to US and sender pays taxes', function () {
-        var result = calc(100.00, 'USD', 'BR', 'US', true);
-        expect(result.received).toBe(100.00);
-        expect(result.taxes).toBe(90.00);
-        expect(result.sent).toBe(110.00);
-    });
-});
